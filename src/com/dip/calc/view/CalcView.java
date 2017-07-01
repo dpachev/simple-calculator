a0e4b5f5d9846c74b213417d99d34f443098dad6
dip641 <dip641@student.bham.ac.uk>
2017-07-01 14:54:07 +0300

    View updates

--- src/com/dip/calc/view/CalcView.java
+++ src/com/dip/calc/view/CalcView.java
@@ -1,7 +1,9 @@
 package com.dip.calc.view;
 
 import java.awt.BorderLayout;
+import java.awt.Button;
 import java.awt.GridBagConstraints;
+import java.awt.GridLayout;
 
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
@@ -24,21 +26,47 @@ public class CalcView {
 		frame = new JFrame("Calculator");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		panel = new JPanel(new BorderLayout());
-		GridBagConstraints p = new GridBagConstraints();
+		// Main panel
+		panel = new JPanel();
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 		
+		// Result Panel
 		resultPanel = new JPanel();
-		
 		JLabel resultTextField = new JLabel("0");
 		resultPanel.add(resultTextField);
 		
-		p.weightx = 0.5;
-		p.weighty = 0.5;
-		p.gridx = 0;
-		p.gridy = 0;
-		p.gridwidth = 1;
+		// Input Panel
+		inputPanel = new JPanel();
+		inputPanel.setLayout(new BoxLayout(inputPanel, BoxLayout.X_AXIS));
 		
-		panel.add(resultPanel, BorderLayout.NORTH);
+		// Input Buttons Panel
+		inputButtonsPanel = new JPanel();
+		inputButtonsPanel.setLayout(new GridLayout(4, 3));
+		inputButtonsPanel.add(new Button("7"));
+		inputButtonsPanel.add(new Button("8"));
+		inputButtonsPanel.add(new Button("9"));
+		inputButtonsPanel.add(new Button("4"));
+		inputButtonsPanel.add(new Button("5"));
+		inputButtonsPanel.add(new Button("6"));
+		inputButtonsPanel.add(new Button("1"));
+		inputButtonsPanel.add(new Button("2"));
+		inputButtonsPanel.add(new Button("3"));
+		inputButtonsPanel.add(new Button(""));
+		inputButtonsPanel.add(new Button("0"));
+		inputButtonsPanel.add(new Button("."));
+		
+		// Operators Panel
+		operatorsPanel = new JPanel();
+		operatorsPanel.setLayout(new BoxLayout(operatorsPanel, BoxLayout.Y_AXIS));
+		operatorsPanel.add(new Button("+"));
+		operatorsPanel.add(new Button("-"));
+		operatorsPanel.add(new Button("="));
+		
+		inputPanel.add(inputButtonsPanel);
+		inputPanel.add(operatorsPanel);
+		
+		panel.add(resultPanel);
+		panel.add(inputPanel);
 		
 		
 		frame.add(panel);
