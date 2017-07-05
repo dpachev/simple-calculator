30b440301b297318edebab111cda7f03bf78190b
dip641 <dip641@student.bham.ac.uk>
2017-07-05 16:18:38 +0300

    Imporeved Model

--- src/com/dip/calc/model/CalcModel.java
+++ src/com/dip/calc/model/CalcModel.java
@@ -16,68 +16,94 @@ public class CalcModel {
 	private States state = States.INITIAL;
 
 	private Operator operator;
+	
+	boolean haveInput;
 
 	public CalcModel() {
-		result = 0;
-		input = 0;
-		operator = Operator.NONE;
+		clear();
 	}
 
 	public int getResult() {
 		return result;
 	}
+	
+	public void clear() {
+		result = 0;
+		input = 0;
+		operator = Operator.PLUS;
+		haveInput = false;
+	}
 
+	
 	public void setInput(int input) {
-		switch (state) {
-		case INITIAL: {
-			this.input = input;
-			state = States.SETINPUT;
-			break;
-		}
-		case SETOPERATION: {
-			this.input = input;
-			state = States.SETINPUT;
-			break;
-		}
-		case RESULT: {
-			this.input = input;
-			state = States.SETINPUT;
-			break;
-		}
-		default: {
-			break;
-		}
-		}
+		this.input = input;
+		haveInput = true;
 	}
+	
+	public void setOperator(Operator op) {
+		if(haveInput) {
+			result = doOperation(result, input, operator);
+		}
+		operator = op;
+		haveInput = false;
+	}
+	
+	public int equals() {
+		result = doOperation(result, input, operator);
+		return result;
+	}
+	
+//	public void setInput(int input) {
+//		switch (state) {
+//		case INITIAL: {
+//			this.input = input;
+//			state = States.SETINPUT;
+//			break;
+//		}
+//		case SETOPERATION: {
+//			this.input = input;
+//			state = States.SETINPUT;
+//			break;
+//		}
+//		case RESULT: {
+//			this.input = input;
+//			state = States.SETINPUT;
+//			break;
+//		}
+//		default: {
+//			break;
+//		}
+//		}
+//	}
 
-	public void setOperator(Operator o) {
-		switch (state) {
-		case INITIAL: {
-			this.operator = o;
-			this.state = States.SETOPERATION;
-			break;
-		}
-		case SETOPERATION: {
-			this.operator = o;
-			this.state = States.SETOPERATION;
-			break;
-		}
-		case SETINPUT: {
-			this.result = doOperation(this.result, this.input, operator);
-			this.operator = o;
-			this.state = States.SETOPERATION;
-			break;
-		}
-		case RESULT: {
-			this.operator = o;
-			this.state = States.SETOPERATION;
-			break;
-		}
-		default: {
-			break;
-		}
-		}
-	}
+//	public void setOperator(Operator o) {
+//		switch (state) {
+//		case INITIAL: {
+//			this.operator = o;
+//			this.state = States.SETOPERATION;
+//			break;
+//		}
+//		case SETOPERATION: {
+//			this.operator = o;
+//			this.state = States.SETOPERATION;
+//			break;
+//		}
+//		case SETINPUT: {
+//			this.result = doOperation(this.result, this.input, operator);
+//			this.operator = o;
+//			this.state = States.SETOPERATION;
+//			break;
+//		}
+//		case RESULT: {
+//			this.operator = o;
+//			this.state = States.SETOPERATION;
+//			break;
+//		}
+//		default: {
+//			break;
+//		}
+//		}
+//	}
 
 	private int doOperation(int val1, int val2, Operator operator) {
 		switch (operator) {
